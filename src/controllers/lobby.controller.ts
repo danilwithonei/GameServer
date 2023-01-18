66187c2fa492536f5f22eb9c27882a853314b321
Danil <danilwithonei@gmail.com>
2023-01-18 14:03:27 +0300

    added create lobby on server and client side

--- src/controllers/lobby.controller.ts
+++ src/controllers/lobby.controller.ts
@@ -1,19 +1,23 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import path from "path";
+import { Room } from "../entities/room";
 import { services } from "../server";
 
 class LobbyController {
     async createLobby(req: Request, res: Response) {
-        console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ message: "Input errors ", errors });
         }
         try {
-            const client = services.createClient(req.body.username);
-
-            res.cookie("id", client.id);
+            const room = new Room(req.body.lobbyName);
+            services.rooms.push(room);
+            const client = services.getClientById(req.body.userId);
+            client.setRoom(room.name, room.uuid);
+            console.log(
+                `client [${client.name}] with id: [${client.id}]\ncreated and join to lobby [${room.name}] with id [${room.uuid}]`,
+            );
             res.sendFile(path.join(__dirname, "../../views/html/gamePage.html"));
         } catch (error) {
             console.log(error);
--- src/entities/client.ts
+++ src/entities/client.ts
@@ -6,6 +6,10 @@ export class Client {
     ws: WebSocket;
     name: string;
     id: string;
+    roomName: string;
+    roomUuid: string;
+    x: Number;
+    y: Number;
 
     constructor(name: string) {
         this.name = name;
@@ -14,5 +18,18 @@ export class Client {
 
     sendSelf(msg: IMessage) {
         this.ws.send(JSON.stringify(msg));
+    }
+
+    setRoom(roomName: string, roomUuid: string) {
+        this.roomName = roomName;
+        this.roomUuid = roomUuid;
+    }
+
+    setName(name: string) {
+        this.name = name;
+    }
+    setXY(x: Number, y: Number) {
+        this.x = x;
+        this.y = y;
     }
 }
